6f7a531d7fb2998221ad772541d59c9de7a338a9
Ridhi05 <91784743+Ridhi05@users.noreply.github.com>
2022-08-05 18:52:02 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_q0NW2shcLF3vY9QDDoJxsp3yoXnJKS4",
+    authDomain: "lets-chat-22c83.firebaseapp.com",
+    databaseURL: "https://lets-chat-22c83-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-22c83",
+    storageBucket: "lets-chat-22c83.appspot.com",
+    messagingSenderId: "347357439541",
+    appId: "1:347357439541:web:301712f5faed9cba03337c"
+  };
+
+  firebase.initializeApp(firebaseConfig);
